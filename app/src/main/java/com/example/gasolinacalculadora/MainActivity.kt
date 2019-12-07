448e271b08d17d6dfd4c3a668b149ce682ec3a05
Fabio Bueno <fabio.bueno@scaffoldeducation.com.br>
2019-12-07 14:44:39 -0300

    Start project

---
+++ app/src/main/java/com/example/gasolinacalculadora/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.gasolinacalculadora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        calcular.setOnClickListener {
+            resultadoConta.text = (gasolina.text.toString().toFloat() * 0.7).toString()
+            Log.i("CALCULADORA", "Valor Gasolina:$gasolina")
+            Log.i("CALCULADORA", "Valor Alcool:$alcool")
+            Log.i("CALCULADORA", "Resultado da Conta:$resultadoConta")
+
+            if (resultadoConta.text.toString().toFloat() > alcool.text.toString().toFloat()){
+                qualComprar.text = "Alcool"
+                Log.i("CALCULADORA", "Compra Alcool")
+            }else{
+                qualComprar.text = "Gasolina"
+                Log.i("CALCULADORA", "Compra Gasolina")
+            }
+        }
+    }
+}
